99c0ad467332eb27055ee2270e18a588d9027857
sgr <253824369@qq.com>
2019-03-21 00:12:08 +0800

    提交DataFrame操作介绍

---
+++ spark-sql/notes/DataFrame相关操作.md
@@ -0,0 +1,140 @@
+Spark SQL DataFrame相关操作
+------
+* [Contents](#Contents)
+	* [打印DataFrame里面的模式](#打印DataFrame里面的模式)
+	* [查询DataFrame里面的数据](#查询DataFrame里面的数据)
+	* [对DataFrame里面的数据进行排序](#对DataFrame里面的数据进行排序)
+	* [对DataFrame列进行重命名](#对DataFrame列进行重命名)
+	* [对DataFrame列进行类型转换](#对DataFrame列进行类型转换)
+	* [对DataFrame删除增加列](#对DataFrame删除增加列)
+	* [对DataFrame列进行正则处理](#对DataFrame列进行正则处理)
+	* [对DataFrame列进行聚合操作](#对DataFrame列进行聚合操作)
+	* [对DataFrame的缺失值(Null)值处理](#对DataFrame的缺失值(Null)值处理)
+	* [对DataFrame进行join操作](#对DataFrame进行join操作)
+	* [对DataFrame进行union操作](#对DataFrame进行union操作)
+	* [DataFrame窗口函数使用](#DataFrame窗口函数使用)
+	* [对DataFrame数据转JSON](#对DataFrame数据转JSON)
+	
+Spark SQL中的DataFrame类似于一张关系型表。在关系型数据库中可以对单表或多表进行查询操作，
+在DataFrame中都通过调用其API层面来实现。可以參考，Scala提供的[DataFrame API](http://spark.apache.org/docs/2.2.0/api/scala/index.html#org.apache.spark.sql.Dataset)。
+## 打印DataFrame里面的模式
+可以通过printSchema函数来查看。它会打印出列的名称和类型，是否能为null
+```scala
+    // 打印schema
+    df.printSchema()
+    // 查看列的类型
+    df.dtypes
+    // 查看列名
+    df.columns
+    // 查看行数
+    df.count
+```
+## 查询DataFrame里面的数据
+可以使用show方法进行简单查看：
+可以指定采样的行数def show(numRows: Int)。不指定指定任何参数，show函数默认会加载出20行的数据def show()
+可以指定一个boolean值，这个值说明是否需要对超过20个字符的列进行截取def show(truncate: Boolean)；
+还可以使用def select(cols: Column*)进行列选择。还可以使用def filter(conditionExpr: String)进行过滤。
+```scala
+    df.select("id", "name", "age").show(3)
+    df.select(df("id"), df("name"), df("age")).show(true)
+    df.select($"id", $"name", $"age").show(3,true)
+```
+## 对DataFrame里面的数据进行排序
+可以使用sort函数对DataFrame中指定的列进行排序。
+```scala
+    // 默认升序
+    df.sort("age").show()
+    // 降序排序
+    df.sort($"age".desc).show()
+    //多字段排序 混合排序
+    df.sort($"id".desc, $"age".asc).show()
+```
+## 对DataFrame列进行重命名
+对DataFrame中默认的列名不感兴趣，我们可以在select的时候利用as对其进行重命名，
+也可以使用def withColumnRenamed(existingName: String, newName: String)方法进行重命名。
+```scala
+    // withColumnRenamed方法 ==> 这种方式会返回新的DF包含所有列  推荐
+    df.withColumnRenamed("id" , "uid").show()
+    // alias 方法 ==> 这种方式会返回新的DF只有uid一列
+    df.select(df("id").alias("uid")).show()
+    df.select(df("id") as "uid", df("id").as("uid2")).show()
+```
+## 对DataFrame列进行类型转换
+如果对拿到的DataFrame列的类型不满意是，可以通过cast进行类型转换。
+```scala
+    df.selectExpr("cast(id as int) as id", "name", "cast(age as int) age")
+    df.select(df("id").cast("int"), df("name"), df("age").cast("int"))
+```
+
+## 对DataFrame删除增加列
+在需要时，可以对DataFrame的列进行删除，新成一个新的DataFrame，也可以主动加入新列。
+```scala
+    // 删除列
+    df.drop("id").show()
+    // 增加列
+    df.withColumn("newAge", df("age") + 10).show()
+    // 增加常量列
+    df.selectExpr("*", "1 one").show()
+```
+
+## 对DataFrame列进行正则处理
+可以使用def regexp_extract(e: Column, exp: String, groupIdx: Int)采用Java正则对列进行处理，
+并返回匹配的指定分组值。也可以使用def regexp_replace(e: Column, pattern: String, replacement: String)
+进行正则替换。
+```scala
+    df.select($"id",
+      regexp_extract($"name", "([A-Za-z]+)(_[0-9]?)?", 1).as("name")
+      , $"age").show()
+    df.select($"id",
+      regexp_replace($"name", "([A-Za-z]+)(_[0-9]+)?", "aaa").as("name")
+      , $"age").show()
+```
+## 对DataFrame列进行聚合操作
+聚合操作调用的是agg方法，该方法有多种调用方式。一般与groupBy方法配合使用。
+```scala
+    df.agg(max("age"), min("age")).show()
+    df.agg(Map("age"->"max", "age"->"min")).show()
+    df.agg(("age", "max"), ("age", "min")).show()
+    // min max count 等函数必须放在agg里
+    df.groupBy("age").agg(min("id")).show()
+```
+## 对DataFrame的缺失值(Null)值处理
+可以使用na.fill(map)，实现对NULL值的填充。
+```scala
+    // null 统一替换成 ""
+    df.na.fill("").show()
+    // 根据不同列进行null值替换
+    df.na.fill(Map("name"->"", "age"-> -1)).show()
+```
+## 对DataFrame进行join操作
+通过join函数实现两个DataFrame的连接操作，并要指定连接字段。默认的连接方式是inner，
+当然也可以使用其他的方式，通过第三个参数来指定，可以指定的类型有`inner`, `cross`,
+`outer`, `full`, `full_outer`, `left`, `left_outer`,`right`, `right_outer`,
+`left_semi`, `left_anti`类型。
+```scala
+    // 默认inner
+    df1.join(df2,"id").show()
+    // `inner`, `cross`, `outer`, `full`, `full_outer`, `left`, `left_outer`,`right`, `right_outer`, `left_semi`, `left_anti`
+    df1.join(df2, Seq("id"), "right").show()
+    // 必须使用===代替等于
+    df1.join(df2, df1("id") === df2("id") ).show()
+```
+## 对DataFrame进行union操作
+注意Spark SQL中union和hql的union不一样，只是合并不会去重相当于SQL中的UNION ALL操作。
+其中unionAll方法底层也是调用union，已经过时。
+```scala
+df1.union(df2).show()
+``` 
+## DataFrame窗口函数使用
+可以对DataFrame使用窗口函数row_number()、rank()等。
+```scala
+    df.select($"id", $"name", $"age",
+      row_number().over(Window.partitionBy("age").orderBy($"id".asc)).as("rank")
+    )
+```
+## 对DataFrame数据转JSON
+可以调用df.toJSON将Dataset[Row]转成Dataset[String] json字符串。
+
+  
+
+
---
+++ spark-sql/src/main/scala/org/spark/notes/DataFrameOperate.scala
@@ -0,0 +1,245 @@
+package org.spark.notes
+
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.junit.{After, Before, Test}
+
+/**
+  *
+  * @author sgr
+  * @version 1.0, 2019-03-19 11:37
+  **/
+
+@Test
+class DataFrameOperate {
+  var spark:SparkSession = null
+  var df:DataFrame = _
+
+  @Before
+  def init() = {
+    spark = SparkSession.builder().appName("DataFrameOperate").master("local[2]").getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+
+
+    df = spark.read.csv("./data/people.csv").toDF("id", "name", "age")
+
+
+    df.printSchema()
+  }
+
+  @Test
+  def metaInfo() = {
+    // 打印schema
+    df.printSchema()
+
+    // 查看列的类型
+    println(df.dtypes.mkString(","))
+
+    // 查看列名
+    println(df.columns.mkString(","))
+
+    // 查看行数
+    println(df.count)
+
+  }
+
+  @Test
+  def columnRename() = {
+    // 使用selectExpr方法 ==> 这种方式会返回新的DF只有uid一列
+    val df1 = df.selectExpr("id as uid")
+    df1.show()
+
+    // withColumnRenamed方法 ==> 这种方式会返回新的DF包含所有列  推荐
+    val df2 = df.withColumnRenamed("id" , "uid")
+    df2.show()
+
+    // alias 方法 ==> 这种方式会返回新的DF只有uid一列
+    val df3 = df.select(df("id").alias("uid"))
+    df3.show()
+
+    df.select(df("id") as "uid", df("id").as("uid2")).show()
+
+  }
+
+  @Test
+  def selection() = {
+    val sql = spark.sqlContext
+    import sql.implicits._
+
+    // 选择几列的方法
+    df.select("id", "name", "age").show(3)
+    df.select(df("id"), df("name"), df("age")).show(true)
+    df.select($"id", $"name", $"age").show(3,true)
+
+    // 多列选择和切片
+    df.select("id", "name", "age").select(df("age") > 10).show()
+    // between 范围选择
+    df.filter(df("age").between(10, 20)).select("id", "name", "age").show()
+    // 联合筛选
+    df.filter(df("age") > 10).filter(df("name").isin("wangwu","lisi")).show()
+
+    // filter运行类SQL
+    df.filter("age > 10 and name in('wangwu', 'lisi')").show()
+
+    // where方法的SQL
+    df.where("age > 10 and name in('wangwu', 'lisi')").show()
+  }
+
+  @Test
+  def modifyColumn() = {
+    val sql = spark.sqlContext
+    import sql.implicits._
+    // 删除列
+    df.drop("id").show()
+
+    // 增加列
+    df.withColumn("newAge", df("age") + 10).show()
+
+    import org.apache.spark.sql.functions._
+    df.withColumn("newAge", col("age") + 10).show()
+
+    // 增加常量列
+    df.selectExpr("*", "1 one").show()
+
+  }
+
+  @Test
+  def toJson() = {
+    val p = (x:String) => println(x)
+    df.toJSON.foreach(p)
+  }
+
+
+  @Test
+  def sort() = {
+    val sql = spark.sqlContext
+    import sql.implicits._
+
+    // 默认升序
+    df.sort("age").show()
+
+    // 降序排序
+    df.sort($"age".desc).show()
+
+    //多字段排序 混合排序
+    df.sort($"id".desc, $"age".asc).show()
+  }
+
+  @Test
+  def dealNull() = {
+
+    // null 统一替换成 ""
+    df.na.fill("").show()
+
+    // 根据不同列进行null值替换
+    df.na.fill(Map("name"->"", "age"-> -1)).show()
+
+    df.selectExpr("id", "coalesce(name, '') as name", "coalesce(age, -1) as age").show()
+
+  }
+
+  @Test
+  def typeCast() = {
+
+    val df1 = df.selectExpr("cast(id as int) as id", "name", "cast(age as int) age")
+
+    val df2 = df.select(df("id").cast("int"), df("name"), df("age").cast("int"))
+
+    df1.printSchema()
+    df1.show()
+
+    df2.printSchema()
+    df2.show()
+  }
+
+  @Test
+  def regexp_op() = {
+    val sql = spark.sqlContext
+    import sql.implicits._
+
+    import org.apache.spark.sql.functions._
+    df.select($"id",
+      regexp_extract($"name", "([A-Za-z]+)(_[0-9]?)?", 1).as("name")
+      , $"age").show()
+    df.select($"id",
+      regexp_replace($"name", "([A-Za-z]+)(_[0-9]+)?", "aaa").as("name")
+      , $"age").show()
+  }
+
+  @Test
+  def agg_op() = {
+    import org.apache.spark.sql.functions._
+    df.agg(max("age"), min("age")).show()
+
+    df.agg(Map("age"->"max", "age"->"min")).show()
+
+    df.agg(("age", "max"), ("age", "min")).show()
+
+    df.groupBy("age").agg(min("id")).show()
+
+    df.createOrReplaceTempView("people")
+
+    spark.sql("select age,min(id) from people group by age").show()
+  }
+
+
+  @Test
+  def window_func_op() = {
+    val sql = spark.sqlContext
+    import sql.implicits._
+    import org.apache.spark.sql.functions._
+    val df1 = df.select($"id", $"name", $"age",
+      row_number().over(Window.partitionBy("age").orderBy($"id".asc)).as("rank")
+    )
+    df1.show()
+
+    df1.filter("rank = 1").show()
+  }
+
+  @Test
+  def join_op() = {
+    val df1 = df.filter("id < 3")
+    val df2 = df.na.drop()
+
+    df1.show()
+    df2.show()
+
+//    df1.join(df2).show()
+
+    // 默认inner
+    df1.join(df2,"id").show()
+
+    // `inner`, `cross`, `outer`, `full`, `full_outer`, `left`, `left_outer`,`right`, `right_outer`, `left_semi`, `left_anti`
+    df1.join(df2, Seq("id"), "right").show()
+
+    // 必须使用===代替等于
+    df1.join(df2, df1("id") === df2("id") ).show()
+
+
+    // 下面操作产生笛卡尔积 需要配置spark.sql.crossJoin.enabled=true 不然会报：
+    // AnalysisException: Detected cartesian product for INNER join between logical plans
+    spark.conf.set("spark.sql.crossJoin.enabled", "true")
+    df1.join(df2, df1("id").>(df2("id"))).show() // 不好使
+
+    df1.createOrReplaceTempView("df1")
+    df2.createOrReplaceTempView("df2")
+    // 建议直接使用sql
+    spark.sql("select * from df1 a inner join df2 b on a.id > b.id").show()
+  }
+
+  @Test
+  def union_op() = {
+    val df1 = df.filter("id < 3")
+    val df2 = df.na.drop()
+
+    df1.show()
+    df2.show()
+    df1.union(df2).distinct().show()
+  }
+
+  @After
+  def shutDown() = {
+    if (spark != null) spark.close()
+  }
+
+}
